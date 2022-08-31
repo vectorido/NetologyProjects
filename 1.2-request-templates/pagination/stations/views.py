ad88613600b3ccb4897cfb6f3ef776d9910fb659
Ilia Chernetsov <93093814+vectorido@users.noreply.github.com>
2022-08-31 22:45:08 +0400

    Update views.py

--- 1.2-request-templates/pagination/stations/views.py
+++ 1.2-request-templates/pagination/stations/views.py
@@ -11,20 +11,14 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    ## form a list of data from csv file
     databank = open('data-bus-stations.csv', 'r', encoding='UTF-8')
     csv_block = csv.DictReader(databank)
     rows = list(csv_block)
-    table = []
-    for row in rows:
-        my_dict = {}
-        for column in row:
-            my_dict[column] = row[column]
-        table.append(my_dict)
     databank.close()
+    ## create pagination
     page_number = int(request.GET.get("page", 1))
-    paginator = Paginator(table, 10)
+    paginator = Paginator(rows, 10)
     page = paginator.get_page(page_number)
     context = {
         'bus_stations': page,
